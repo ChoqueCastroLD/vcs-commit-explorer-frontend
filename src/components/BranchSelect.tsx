ba2412ac58e515b9d6db813024bb9169dfeacdca
Luis Choque <luis.choque.castro@outlook.com>
2023-10-14 16:32:17 -0500

    Improved branch select

--- src/components/BranchSelect.tsx
+++ src/components/BranchSelect.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 const BranchSelect = ({ branches, selectedBranch, onBranchSelect }) => {
   return (
     <select
-      className="select select-secondary w-full max-w-xs"
+      className="select select-secondary w-full max-w-xs m-4"
       value={selectedBranch}
       onChange={(e) => onBranchSelect(e.target.value)}
     >
       <option disabled>Select a branch</option>
-      {branches.map((branch) => (
+      {branches.map((branch: { name: string }) => (
         <option key={branch.name} value={branch.name}>
           {branch.name}
         </option>
