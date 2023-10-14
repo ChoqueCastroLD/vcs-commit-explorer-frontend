9856eb180936b6cf37c1bf9a8b84bf113dd82493
Luis Choque <luis.choque.castro@outlook.com>
2023-10-14 16:32:56 -0500

    Added api service

---
+++ src/services/api.ts
@@ -0,0 +1,31 @@
+const BASE_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000';
+
+async function fetchJSON(url: string) {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch data. Status: ${response.status}`);
+    }
+
+    const contentType = response.headers.get('content-type');
+    if (contentType?.includes('application/json')) {
+        return response.json();
+    } else {
+        throw new Error('Invalid response format: Expected JSON');
+    }
+}
+
+export default {
+    async fetchRepository(vcs: string, owner: string, repository: string): Promise<any> {
+        const apiUrl = `${BASE_URL}/api/${vcs}/${owner}/${repository}`;
+        return fetchJSON(apiUrl);
+    },
+    async fetchBranches(vcs: string, owner: string, repository: string) {
+        const apiUrl = `${BASE_URL}/api/${vcs}/${owner}/${repository}/branches`;
+        return fetchJSON(apiUrl);
+    },
+    async fetchCommits(vcs: string, owner: string, repository: string, sha: string) {
+        const apiUrl = `${BASE_URL}/api/${vcs}/${owner}/${repository}/commits/${sha}`;
+        return fetchJSON(apiUrl);
+    }
+};
